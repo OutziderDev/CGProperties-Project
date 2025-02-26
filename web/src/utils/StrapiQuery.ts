24fb4cbe7daa7b7f65f5196299c7f88f7892211e
Luis <luisbarrios0110@gmail.com>
2025-02-26 15:45:02 -0500

    Generando la informacion directa en la web desde Strapi

--- web/src/utils/StrapiQuery.ts
+++ web/src/utils/StrapiQuery.ts
@@ -1,7 +1,7 @@
 const  STRAPI_URL = import.meta.env.PUBLIC_STRAPI_URL;
 const STRAPI_TOKEN  = import.meta.env.PUBLIC_STRAPI_TOKEN;
-console.log('url', STRAPI_URL)
-export function query(url) {
+
+export function query(url: String) {
   return fetch(`${STRAPI_URL}/api/${url}`, {
     headers: {
       Authorization: `Bearer ${STRAPI_TOKEN}`
--- web/src/utils/getContactInfo.ts
+++ web/src/utils/getContactInfo.ts
@@ -2,9 +2,6 @@ import { query } from "./StrapiQuery";
 
 export function getContactInfo() {
   return query("contacto")
-    .then(res => {
-      console.log(res)
-      return res
-    }
+    .then(res => { return res }
   )
 }
