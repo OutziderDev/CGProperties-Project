b79fddd63aa1c1dc395da38256ed49aacedc0ece
Luis <luisbarrios0110@gmail.com>
2025-03-13 19:53:35 -0500

    Fix: Cancelando el guardado de cache por parte de vercel

--- web/src/utils/StrapiQuery.ts
+++ web/src/utils/StrapiQuery.ts
@@ -4,8 +4,8 @@ const STRAPI_TOKEN  = import.meta.env.PUBLIC_STRAPI_TOKEN;
 export function query(url: String) {
   return fetch(`${STRAPI_URL}/api/${url}`, {
     headers: {
-      Authorization: `Bearer ${STRAPI_TOKEN}`
+      Authorization: `Bearer ${STRAPI_TOKEN}`,
+      cache: "no-store"
     }
   }).then(res => res.json())
 } 
-
